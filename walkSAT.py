4ff38a7d05cbe628ea2b1d673ab531a28dc361b0
Young-Taek Oh <ofrank23447@gmail.com>
2018-11-29 23:26:36 -0800

    added updated walkSAT.py

--- walkSAT.py
+++ walkSAT.py
@@ -1,9 +1,12 @@
 import time
-
+import clause
+import os
+import matplotlib.pyplot as plt
 
 import random
 
 def getClauses_Symbols(cnf):
+    global symLength
     clauses = []
     symbols = set()
     split_clauses = cnf.split("),")
@@ -15,6 +18,7 @@ def getClauses_Symbols(cnf):
             symbols.add(int(symbol))
 
         clauses.append(new_clause)
+    symLength = len(symbols)
     return clauses, symbols    
 
 
@@ -26,10 +30,10 @@ def prop_symbols(clause):
 
 #evaluate a propositional logical sentence in a model
 def pl_true(clause,model):
-    print("clause:",clause)
-    print("model:",model)
+    #print("clause:",clause)
+    #print("model:",model)
     #returns true if the propositional logical expression is true in the model 
-    if len([symbol for symbol in clause if model[symbol] is True]) == 0:
+    if len([symbol for symbol in clause if model[int(symbol)] is True]) == 0:
         return False
     return True
 
@@ -39,12 +43,12 @@ def probability(p):
     return p > random.uniform(0.0, 1.0)
 
 def walkSAT_satisfiable(cnf):
-    print(cnf)
+    #print(cnf)
     clauses,symbols = getClauses_Symbols(cnf)
     #probability of choosing to do a "random walk" move
     p = 0.5
-    print("clauses:", clauses)
-    print("symbols:", symbols)
+    #print("clauses:", clauses)
+    #print("symbols:", symbols)
     #number of flips allowed before giving up 
     max_flips = 10000
     return walkSAT(clauses,symbols,p,max_flips)
@@ -56,7 +60,7 @@ def walkSAT(clauses,symbols,p,max_flips):
 
     
     for i in range(max_flips):
-        print(i)
+        #print("Loop:",i)
         satisfied, unsatisfied = [], []
         for clause in clauses:
             (satisfied if pl_true(clause, model) else unsatisfied).append(clause)
@@ -68,22 +72,51 @@ def walkSAT(clauses,symbols,p,max_flips):
         else:
             #Flip the symbol in clause that maximizes number of sat. clauses
             def sat_count(sym):
+                sym = int(sym)
                 #Return the number of clauses satisfied after flipping the symbol
+                #print("sym:",type(sym))
                 model[sym] = not model[sym]
                 count = len([clause for clause in clauses if pl_true(clause,model)])
                 model[sym] = not model[sym]
                 return count
             sym = max(prop_symbols(clause), key=sat_count)
+        sym = int(sym)
         model[sym] = not model[sym]
     # If no solution is found within the flip limit, we return failure
     return None 
 
+def output(model):
+    print("")
+    
+
 
 
 if __name__ == "__main__":
-    start_time = time.time()
-    cnf = "(3,-5,6),(-9,2,1)"
+    #start_time = time.time()
+    #cnf = "(3,-5,6),(-9,2,1)"
+    files = [f for f in os.listdir('satisfy2')]
+    x_vals = range(len(files))
+    #range(len(files))
+    y_vals = []
+    sat_count = 0
+    unsat_count = 0 
+
+    for f in files:
+        start_time = time.time()
+        cnf = clause.giveInput("satisfy2/" + f)
+        if walkSAT_satisfiable(cnf):
+            print("All True!" + f + "\n")
+            sat_count += 1
+        else:
+            print("UNSATISFIABLE" + f + "\n")
+            unsat_count += 1
+        end_time = time.time()
+        y_vals.append(end_time - start_time)
+    #print("Elapsed Time: %s seconds" % (end_time - start_time))
     
-    walkSAT_satisfiable(cnf)
-    end_time = time.time()
-    print("Elapsed Time: %s seconds" % (end_time - start_time))
+    print("total count: " + str(len(files)))
+    print("satisfiable count: " + str(sat_count))
+    print("unsatisfiable count: " + str(unsat_count))
+    print("average time: "+ str(sum(y_vals)/len(y_vals)))
+    plt.plot(x_vals, y_vals)
+    plt.show()
